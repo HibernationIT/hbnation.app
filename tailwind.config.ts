a7ee1822f55f29b0e2031bfe078113d63e9e2955
hbnationit <hbnation.it@gmail.com>
2024-03-13 08:35:55 +0900

    프로젝트 스타일 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,28 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: "media",
   content: [
-    "./node_modules/flowbite-react/lib/**/*.js",
+    "./node_modules/flowbite/**/*.js",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./public/**/*.html",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        main: {
+          100: "#E6CFFF",
+          200: "#CB99FF",
+          300: "#AA5CFB",
+          400: "#8942D3",
+          500: "#692CA9",
+          600: "#4A0083",
+          700: "#2E005D",
+          800: "#1D013A",
+          900: "#120024",
+        },
       },
     },
   },
