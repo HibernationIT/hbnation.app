74e12fb7c1f92782ab2363003c1a860b2e796fda
hbnationit <hbnation.it@gmail.com>
2024-04-16 00:53:04 +0900

    Project 내부 페이지를 SCSS CSS 모듈로 전환

--- src/components/atom/heading.tsx
+++ src/components/atom/heading.tsx
@@ -17,13 +17,7 @@ export default function Heading({ value }: { value: Headings }) {
               <svg
                 viewBox="0 0 100 100"
                 style={{
-                  width: "0.6875em",
-                  height: "0.6875em",
-                  display: "block",
-                  flexShrink: 0,
-                  transition: "transform 200ms ease-out 0s",
                   transform: `rotateZ(${open ? 180 : 90}deg)`,
-                  opacity: 1,
                 }}
               >
                 <polygon points="5.9,88.2 50,11.8 94.1,88.2 "></polygon>
